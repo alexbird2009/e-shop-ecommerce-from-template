cfc435506feb31380ef8fb0f761d913caa8f1b52
Alex <56090574+alexbird2009@users.noreply.github.com>
2021-05-19 01:16:56 +0300

    create products.js, start products interactivity

---
+++ js/products.js
@@ -0,0 +1,11 @@
+const data = [
+    {
+        imgSrc: "./images/product_1.jpg",
+        name: "Woman's Long Dress",
+    },
+];
+
+function loadProducts() {
+    alert("Loaded!");
+}
+window.addEventListener("load", loadProducts);
